d30a4d97bb027c814bdba540ff867f05f15b4645
Diego G. Slomp <diegogslomp@gmail.com>
2015-03-30 17:00:40 -0300

    added fieldsets to class HostAdmin in admin.py

--- monitor/admin.py
+++ monitor/admin.py
@@ -6,7 +6,18 @@ class HostAdmin(admin.ModelAdmin):
 
     list_display =  ('name','description','ipv4','status')
     search_fields = ['name','description','ipv4','status']
-
+    
+    fieldsets = (
+    
+        (None, {
+            'fields': ('name', 'description', 'ipv4', 'services', )
+        }),
+        
+        ('Daemon managed options', {
+            'classes': ('collapse',),
+            'fields': ('services_info', 'last_check', 'status',)
+        }),
+    )
 
 admin.site.register(Host, HostAdmin)
 admin.site.register(Service)
