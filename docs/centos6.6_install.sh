2a601d15aca6ba8d9c37366b894595045a308a79
Diego G. Slomp <diegogslomp@gmail.com>
2015-04-02 12:24:14 -0300

    centos script updated

--- docs/centos6.6_install.sh
+++ docs/centos6.6_install.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
-#Installs Python 2.7.9 on a Centos6.6 fresh install 
+#Installs pyenv on a Centos6.6 fresh install 
 
-ifup eth0
-iptables -F
-yum update -y
-yum groupinstall -y 'development tools'
-yum install -y zlib-devel bzip2-devel openssl-devel xz-libs wget sqlite-devel
-wget https://www.python.org/ftp/python/2.7.9/Python-2.7.9.tar.xz
-xz -d Python-2.7.9.tar.xz
-tar -xvf Python-2.7.9.tar
-cd Python-2.7.9
-./configure --prefix=/usr/local
-make
-make altinstall
-export PATH="/usr/local/bin:$PATH"
-curl https://raw.githubusercontent.com/pypa/pip/master/contrib/get-pip.py | python2.7 -
-pip2.7 install django
+sudo ifup eth0
+sudo iptables -F
+sudo yum update -y
+curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bash_profile
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bash_profile
+echo 'eval "$(pyenv init -)"' >> ~/.bash_profile
+source ~/.bash_profile
+pyenv install 3.4.3
+pyenv global 3.4.3
 
-#Remember to use python2.7 e pip2.7 to install and run django commands, or make alias for them
+# Reboot to don't type `source ~/.bash_profile` for every shell opened
