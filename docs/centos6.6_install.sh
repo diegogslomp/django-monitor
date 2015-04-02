30974bd3ae058f4b9e68d2c24d68da5b76e3a055
Diego G. Slomp <diegogslomp@gmail.com>
2015-04-02 14:21:09 -0300

    centos script updated and working

--- docs/centos6.6_install.sh
+++ docs/centos6.6_install.sh
@@ -4,12 +4,15 @@
 sudo ifup eth0
 sudo iptables -F
 sudo yum update -y
+
 curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bash_profile
 echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bash_profile
 echo 'eval "$(pyenv init -)"' >> ~/.bash_profile
 source ~/.bash_profile
+
 pyenv install 3.4.3
 pyenv global 3.4.3
+pip install django
 
-# Reboot to don't type `source ~/.bash_profile` for every shell opened
+# Reboot or type `source ~/.bash_profile` for every shell opened until reboot.
