2e612c2caaaa865b47d024b398ac1ee62d50efbd
Diego G. Slomp <diegogslomp@gmail.com>
2015-04-05 17:04:13 -0300

    Update and rename centos6.6_install.sh to centos_install.sh
    - added ubuntu comments
    - make ubuntu install script

--- docs/centos_install.sh
+++ docs/centos_install.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-#Installs pyenv on a Centos6.6 fresh install 
+#Installs pyenv on a Centos
 
-sudo ifup eth0
-sudo iptables -F
-sudo yum update -y
+yum update -y
+yum install -y zlib-devel bzip2 bzip2-devel readline-devel sqlite sqlite-devel openssl-devel
 
 curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bash_profile
@@ -16,3 +15,6 @@ pyenv global 3.4.3
 pip install django
 
 # Reboot or type `source ~/.bash_profile` for every shell opened until reboot.
+# For Ubuntu/Debian:
+## sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm
+## edit .basrc instead .bash_profile
