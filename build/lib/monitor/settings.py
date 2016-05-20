b59cf6e62868f133b604084c0e5b037017f92c6e
Diego G. Slomp <diegogslomp@gmail.com>
2016-05-20 09:22:13 -0300

    pmcs version, without services

---
+++ build/lib/monitor/settings.py
@@ -0,0 +1,6 @@
+# Days to change the status from red (danger) to yellow (warning)
+DAYS_FROM_DANGER_TO_WARNING = 5
+# Days to change the status from blue (info) to green (success)
+DAYS_FROM_INFO_TO_SUCCESS = 1
+#Time to wait after each host check in seconds (0..) 
+WAIT_FOR_NEXT = 1
