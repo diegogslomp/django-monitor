b9286444d4a9fe420ae029d9078d334490f11423
Diego G. Slomp <diegogslomp@gmail.com>
2016-08-11 11:19:48 -0300

    build/ removed

--- build/lib/monitor/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Host
-
-
-class HostAdmin(admin.ModelAdmin):
-    list_display = ('name', 'description', 'ipv4', 'status')
-    search_fields = ['name', 'description', 'ipv4', 'status']
-
-admin.site.register(Host, HostAdmin)
