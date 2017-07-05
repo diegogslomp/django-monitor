1c4c673f63b65afceb0ca356887b00f4c98a222f
Diego Gobbi Slomp <diegogslomp@gmail.com>
2017-07-05 08:36:08 -0300

    new version detail jsonView

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-monitor',
-    version='0.1.9',
+    version='0.1.10',
     packages=['monitor'],
     include_package_data=True,
     license='MIT License',
