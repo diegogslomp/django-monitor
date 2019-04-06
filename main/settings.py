e2874bf9abbb0854cf83359c4c2ceff838bac894
Diego Gobbi Slomp <diegogslomp@gmail.com>
2019-04-06 13:59:55 -0300

    fix db config

--- main/settings.py
+++ main/settings.py
@@ -78,7 +78,8 @@ DATABASES = {
     'default': {
         'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.sqlite3'),
         'NAME': os.getenv('DB_NAME', os.path.join(BASE_DIR, 'db.sqlite3')),
-        'USER': os.getenv('DB_PASSWORD', ''),
+        'USER': os.getenv('DB_USER', ''),
+        'PASSWORD': os.getenv('DB_PASSWORD', ''),
         'HOST': os.getenv('DB_HOST',''),
         'PORT': os.getenv('DB_PORT',''),
     },
