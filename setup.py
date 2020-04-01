b4d904ed3a12b071bea1f2402591b4a128601b64
Diego Gobbi Slomp <diegogslomp@gmail.com>
2020-03-31 22:36:33 -0300

    setup.cfg inside setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,37 @@
+import os
 from setuptools import setup
 
-setup()
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-monitor',
+    version='0.4.0',
+    packages=['monitor'],
+    include_package_data=True,
+    license='MIT License',
+    description='Ping devices and check router/switch port status. Useful in ring topology networks.',
+    long_description=README,
+    url='https://github.com/diegogslomp/django-monitor/',
+    author='Diego Gobbi Slomp',
+    author_email='diegogslomp@gmail.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: System :: Monitoring',
+        'Topic :: System :: Networking :: Monitoring',
+    ],
+    keywords='monitor ping icmp telnet',
+    install_requires=[
+        'django',
+        'gunicorn',
+        'psycopg2-binary',
+    ],
+)
