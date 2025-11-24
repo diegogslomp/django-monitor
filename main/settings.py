6aea976085b973c318cf40db7e7ef2c3b76feea0
Diego Gobbi Slomp <diegogslomp@gmail.com>
2025-11-24 09:42:13 -0300

    Add dotenv to read .env file, alternative to pipenv

--- main/settings.py
+++ main/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import dotenv
 import os
+
+dotenv.load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
